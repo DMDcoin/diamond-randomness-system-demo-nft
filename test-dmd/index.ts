b95bd79ce738f343096d6a33497271652db44ece
Thomas Haller <thomashaller@gmx.at>
2023-01-31 20:21:34 +0100

    added all tests that should suceed on real diamond networks.

--- test-dmd/index.ts
+++ test-dmd/index.ts
@@ -57,126 +57,64 @@ describe("NFT", function () {
     await nft?.deployed();
   });
 
-  // it("minting should fail if not registered", async () => {
-  //   if (nft) {
-  //     await expect(nft.mintTo(main)).revertedWith("minting not registered");
-  //   }
-  // });
+  it("minting should fail if not registered", async () => {
+    if (nft) {
+      await expect(nft.mintTo(main)).revertedWith("minting not registered");
+    }
+  });
 
-  // it("registering minting should fail if not enough minting fee is provided.", async () => {
-  //   if (nft) {
-  //     await expect(nft.registerMinting(main)).revertedWith(
-  //       "must send exact minting fee"
-  //     );
-  //   }
-  //   // console.log("mintTX:", mintTX?.hash);
-  // });
+  it("registering minting should fail if not enough minting fee is provided.", async () => {
+    if (nft) {
+      await expect(nft.registerMinting(main)).revertedWith(
+        "must send exact minting fee"
+      );
+    }
+  });
 
-  // it("registering minting should succeed if  enough minting fee is provided.", async () => {
-  //   if (nft) {
-  //     await nft.registerMinting(main, registrationFee);
-  //   }
-  //   // console.log("mintTX:", mintTX?.hash);
-  // });
+  it("registering minting should succeed if  enough minting fee is provided.", async () => {
+    if (nft) {
+      await nft.registerMinting(main, registrationFee);
+    }
+    // console.log("mintTX:", mintTX?.hash);
+  });
 
-  // it("registering minting should fail if already registered.", async () => {
-  //   if (nft) {
-  //     await expect(nft.registerMinting(main, registrationFee)).revertedWith(
-  //       "minting already registered"
-  //     );
-  //   }
-  //   // console.log("mintTX:", mintTX?.hash);
-  // });
+  it("registering minting should fail if already registered.", async () => {
+    if (nft) {
+      await expect(nft.registerMinting(main, registrationFee)).revertedWith(
+        "minting already registered"
+      );
+    }
+    // console.log("mintTX:", mintTX?.hash);
+  });
 
-  // it("minting should succeed if registered.", async () => {
-  //   if (nft) {
-  //     await nft.mintTo(main);
-  //     const tokenID = 1;
-  //     const owner = await nft.ownerOf(tokenID);
-  //     expect(owner).to.equal(main);
-  //     const tokenDna = await nft.tokenDna(tokenID);
-  //     expect(tokenDna).not.to.equal(
-  //       "0x0000000000000000000000000000000000000000000000000000000000000000"
-  //     );
-  //   }
-  // });
+  it("minting should succeed if registered.", async () => {
+    if (nft) {
+      await nft.mintTo(main);
+      const tokenID = 1;
+      const owner = await nft.ownerOf(tokenID);
+      expect(owner).to.equal(main);
+      const tokenDna = await nft.tokenDna(tokenID);
+      expect(tokenDna).not.to.equal(
+        "0x0000000000000000000000000000000000000000000000000000000000000000"
+      );
+    }
+  });
 
-  // it("minting registration and minting for other accounts should be possible", async () => {
-  //   if (nft && nftPayerAccount && nftServiceAccount) {
-  //     await nft
-  //       .connect(nftPayerAccount)
-  //       .registerMinting(nftReveiverAccount?.address!, registrationFee);
+  it("minting registration and minting for other accounts should be possible", async () => {
+    if (nft && nftPayerAccount && nftServiceAccount) {
+      await nft
+        .connect(nftPayerAccount)
+        .registerMinting(nftReveiverAccount?.address!, registrationFee);
 
-  //     await nft.connect(nftServiceAccount).mintTo(nftReveiverAccount?.address!);
+      await nft.connect(nftServiceAccount).mintTo(nftReveiverAccount?.address!);
 
-  //     // reveiver have never sent a transaction.
-  //     expect(
-  //       await ethers.provider.getTransactionCount(nftReveiverAccount?.address!)
-  //     ).to.equal(0);
+      // reveiver have never sent a transaction.
+      expect(
+        await ethers.provider.getTransactionCount(nftReveiverAccount?.address!)
+      ).to.equal(0);
 
-  //     // ... but it still owns an NFT.
-  //     expect(await nft.ownerOf(2)).to.equal(nftReveiverAccount?.address!);
-  //   }
-  // });
-
-  // it("minting registration not possible during unhealthy network state", async () => {
-  //   if (nft && nftPayerAccount && nftServiceAccount) {
-  //     await health?.setHealth(false);
-
-  //     await expect(
-  //       nft.registerMinting(nftReveiverAccount?.address!, registrationFee)
-  //     ).revertedWith("Service is currently paused");
-  //   }
-  // });
-
-  // it("minting not possible during unhealthy network state", async () => {
-  //   if (nft && nftPayerAccount && nftServiceAccount) {
-  //     await health?.setHealth(true);
-  //     await nft.registerMinting(nftReveiverAccount?.address!, registrationFee);
-  //     await health?.setHealth(false);
-  //     await expect(nft.mintTo(nftReveiverAccount?.address!)).revertedWith(
-  //       "No Healthy RNG on this Block"
-  //     );
-  //   }
-  // });
-
-  // it("rescheduling minting not possible if network is still unhealthy", async () => {
-  //   if (nft) {
-  //     await expect(
-  //       nft.rescheduleUnhealthyMintRegistration(nftReveiverAccount?.address!)
-  //     ).revertedWith("network needs to be healthy");
-  //   }
-  // });
-
-  // it("rescheduling minting possible if network becomes healthy", async () => {
-  //   if (nft) {
-  //     await health?.setHealth(true);
-  //     await nft.rescheduleUnhealthyMintRegistration(
-  //       nftReveiverAccount?.address!
-  //     );
-  //   }
-  // });
-
-  // it("minting rescheduled registrations is possible", async () => {
-  //   if (nft) {
-  //     await nft.mintTo(nftReveiverAccount?.address!);
-  //   }
-  // });
-
-  // it("minting twice after rescheduling registration is not possible", async () => {
-  //   if (nft) {
-  //     await expect(nft.mintTo(nftReveiverAccount?.address!)).revertedWith(
-  //       "minting not registered"
-  //     );
-  //   }
-  // });
-
-  // it("rescheduling minting not possible is the network is healthy", async () => {
-  //   if (nft) {
-  //     await nft.registerMinting(nftReveiverAccount?.address!, registrationFee);
-  //     await expect(
-  //       nft.rescheduleUnhealthyMintRegistration(nftReveiverAccount?.address!)
-  //     ).revertedWith("already healthy registered");
-  //   }
-  // });
+      // ... but it still owns an NFT.
+      expect(await nft.ownerOf(2)).to.equal(nftReveiverAccount?.address!);
+    }
+  });
 });
